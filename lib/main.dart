8577efddc76ebe170c94bef37ed0392116ddc378
Paulo Victor <paulovictor.souza@wakke.co>
2023-07-23 01:31:22 -0300

    feat/splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:app/views/login.dart';
+import 'package:app/views/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +11,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/login': (context) => const Login(),
+        });
   }
 }
